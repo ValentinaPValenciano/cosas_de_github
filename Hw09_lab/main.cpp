7bb742e8337cb9a82ef8036e25c3f529c69e0d96
ValentinaPValenciano <64869043+ValentinaPValenciano@users.noreply.github.com>
2020-05-29 21:15:19 -0500

    Update main.cpp

--- Hw09_lab/main.cpp
+++ Hw09_lab/main.cpp
@@ -1,24 +1,14 @@
-#include <iostream>
-using namespace std;
+#include "funciones.h"
 
-// Función que retorna un array utilizando el apuntador (*)
-double * suma(double a[2], double b[2]){
-    double * c = new double[2];
-    c[0] = a[0] + b[0];
-    c[1] = a[1] + b[1];
-    return c;
-}
+int main(void) 
+{ 
+    printf("\nThe value of y at x is : %f", 
+             rungeKutta(0,8.5, x, 0.1, dydt);
+        return 0;
+} 
 
-int main(){
-    // Creamos dos arrays que contengan números de doble precisión
-    double X[2] = {3.24, -4.333};
-    double Y[2] = {6.76, -5.667};
-    
-    // Generamos un tercer array con el apuntador (*)
-    double * Z = suma(X, Y);
-    
-    // Imprimirmos los dos elementos de Z.
-    for(int i=0; i<2; i++)
-        cout << Z[i] << endl;
-    return 0;
-}
+{ 
+    printf("\nThe value of y at x is : %f", 
+             rungeKutta(0,8.5, x, 0.1, dvdt);
+        return 0;
+} 
